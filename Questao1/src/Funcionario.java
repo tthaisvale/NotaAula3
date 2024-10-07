99b37e6f8f1e620a2a015c8cf321b833aac06a30
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:44:59 -0300

    Funcionario.java

--- Questao1/src/Funcionario.java
+++ Questao1/src/Funcionario.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.Period;
+
 
 public class Funcionario {
+    protected String nome;
+    protected LocalDate nasc;
+    protected double salario;
+
+    // Construtor
+    public Funcionario(String nome, LocalDate nasc, double salario) {
+        this.nome = nome;
+        this.nasc = nasc;
+        this.salario = salario;
+    }
+
+    // Método para exibir o salário
+    public void informarSalario() {
+        System.out.println("Salário: R$ " + salario);
+    }
+
+    // Método para calcular e exibir a idade do funcionário
+    public void calcularIdade() {
+        LocalDate hoje = LocalDate.now();
+        Period idade = Period.between(nasc, hoje);
+        System.out.println("Idade: " + idade.getYears() + " anos");
+    }
 }
