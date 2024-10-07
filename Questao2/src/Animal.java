9ae5f46c59428160afd7fc8a3b9ca51446a2d87d
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:41:07 -0300

    Animal.java

--- Questao2/src/Animal.java
+++ Questao2/src/Animal.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Animal {
+    protected String nome;
+    protected String raca;
+
+    // Construtor sem parâmetros
+    public Animal() {
+        this.nome = "Desconhecido";
+        this.raca = "Desconhecida";
+    }
+
+    // Construtor com nome
+    public Animal(String nome) {
+        this.nome = nome;
+        this.raca = "Desconhecida";
+    }
+
+    // Método que simula o animal caminhando
+    public void caminha() {
+        System.out.println(this.nome + " está caminhando.");
+    }
 }
