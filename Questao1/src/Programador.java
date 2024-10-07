7ebe353fc2322300e48a59e25352e94e8fd49a47
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:45:46 -0300

    Programador.java

--- Questao1/src/Programador.java
+++ Questao1/src/Programador.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class Programador {
+public class Programador extends Funcionario {
+    private String linguagem;
+
+    // Construtor
+    public Programador(String nome, LocalDate nasc, double salario, String linguagem) {
+        super(nome, nasc, salario);  // Chama o construtor da classe base
+        this.linguagem = linguagem;
+    }
+
+    // Método para exibir a linguagem de programação do programador
+    public void informarLinguagem() {
+        System.out.println("Linguagem de programação: " + linguagem);
+    }
 }
