c64899ff20cc30e48c3a12f785e0d1ecdfb31661
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:42:02 -0300

    Gato.java

--- Questao2/src/Gato.java
+++ Questao2/src/Gato.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public class Gato extends Animal {
 
-public class Gato {
+    // Construtor que chama o construtor da classe base
+    public Gato(String nome) {
+        super(nome);  // Chama o construtor da classe Animal
+    }
+
+    // Método específico para Gato
+    public void mia() {
+        System.out.println(this.nome + " está miando.");
+    }
 }
