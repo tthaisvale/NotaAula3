4d3320d380e5ae38cef24e2ccfb3c2ceb3afa237
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:43:59 -0300

    Commit Inicial

--- Questao2/src/SistemaAnimais.java
+++ Questao2/src/SistemaAnimais.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class SistemaAnimais {
+    public static void main(String[] args) {
+        // Criando um Cachorro
+        Cachorro cachorro = new Cachorro("Tufinho");
+        cachorro.caminha();
+        cachorro.late();
+
+        // Criando um Gato
+        Gato gato = new Gato("Fantasma");
+        gato.caminha();
+        gato.mia();
+    }
 }
