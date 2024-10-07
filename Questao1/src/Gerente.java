24ed1cce0351897f448a792863b7c46401402d3b
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:45:25 -0300

    Gerente.java

--- Questao1/src/Gerente.java
+++ Questao1/src/Gerente.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class Gerente {
+public class Gerente extends Funcionario {
+    private String projeto;
+
+    // Construtor
+    public Gerente(String nome, LocalDate nasc, double salario, String projeto) {
+        super(nome, nasc, salario);  // Chama o construtor da classe base
+        this.projeto = projeto;
+    }
+
+    // Método para exibir o projeto que o gerente está gerenciando
+    public void informarProjeto() {
+        System.out.println("Projeto gerenciado: " + projeto);
+    }
 }
